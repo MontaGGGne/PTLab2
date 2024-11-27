0d12d03b0fc468e19acdbf2170c94e57ac9c7d7b
Shchukin Alexandr Andreevich <shchukinaa2002@gmail.com>
2024-11-27 20:28:38 +0300

    Shchukin: lab2 without variant

--- shop/views.py
+++ shop/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 class PurchaseCreate(CreateView):
     model = Purchase
-    fields = ['product', 'person', 'address']
+    fields = ['product', 'product_quantity', 'person', 'address']
 
     def form_valid(self, form):
         self.object = form.save()
