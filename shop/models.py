60206228df730fcef940faf46fed765f4e3712e3
Shchukin Alexandr Andreevich <shchukinaa2002@gmail.com>
2024-12-05 12:56:14 +0300

    Shchukin: add ci/cd script

--- shop/models.py
+++ shop/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     product_quantity = models.PositiveIntegerField(default=1)
-    order_amount = models.PositiveIntegerField(default=0)
+    # order_amount = models.PositiveIntegerField(default=0)
     person = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
